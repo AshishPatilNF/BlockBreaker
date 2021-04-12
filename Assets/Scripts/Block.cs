3982447f3dfa50d7b8c9a2f65ffca94dff16d50c
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-04-12 22:07:34 +0530

    Block health and block destruction implemented

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -10,25 +10,49 @@ public class Block : MonoBehaviour
     [SerializeField]
     GameObject particleVFX;
 
+    [SerializeField]
+    private int blockHealth = 2;
+
+    [SerializeField]
+    Sprite[] sprite;
+
     private Level level;
+
+    SpriteRenderer spriteRender;
 
     private GameStatus gameStatus;
 
     private void Start()
     {
+        spriteRender = GetComponent<SpriteRenderer>();
         gameStatus = FindObjectOfType<GameStatus>();
         level = FindObjectOfType<Level>();
-        level.AddBlocks();
+
+        if (CompareTag("Breakable"))
+        {
+            level.AddBlocks();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        PreAndPostDestroy();
+        if (CompareTag("Breakable"))
+        {
+            AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
+            if (blockHealth < 1)
+            {
+                PreAndPostDestroy();
+            }
+            else
+            {
+                spriteRender.sprite = sprite[blockHealth-1];
+                blockHealth--;
+            }
+        }
     }
 
     private void PreAndPostDestroy()
     {
-        AudioSource.PlayClipAtPoint(blockSound, Camera.main.transform.position);
         gameStatus.AddScore();
         level.RemoveBlock();
         GameObject particlevfx = Instantiate(particleVFX, transform.position, Quaternion.identity);
